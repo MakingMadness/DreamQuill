cc96ed52e810558535e1566f8efec1100b1a1544
MakingMadness <MakingMadness@users.noreply.github.com>
2023-03-31 05:00:32 +0100

    Save when settings are changed and remove the save button.

--- app/src/main/java/com/makingmadness/dreamquill/SettingsActivity.kt
+++ app/src/main/java/com/makingmadness/dreamquill/SettingsActivity.kt
@@ -2,7 +2,8 @@ package com.makingmadness.dreamquill
 
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.widget.Button
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.security.crypto.EncryptedSharedPreferences
@@ -12,10 +13,9 @@ class SettingsActivity : AppCompatActivity() {
 
     private lateinit var apiKeyEditText: EditText
     private lateinit var timeoutEditText: EditText
-    private lateinit var saveButton: Button
 
     private lateinit var encryptedSharedPreferences: SharedPreferences
-    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var timeoutSharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,19 +32,16 @@ class SettingsActivity : AppCompatActivity() {
             EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
         )
 
-        sharedPreferences = getSharedPreferences("DREAMQUILL_PREFS", MODE_PRIVATE)
+        timeoutSharedPreferences = getSharedPreferences("DREAMQUILL_PREFS", MODE_PRIVATE)
 
         apiKeyEditText = findViewById(R.id.api_key_edit_text)
         timeoutEditText = findViewById(R.id.timeout_edit_text)
-        saveButton = findViewById(R.id.save_button)
 
         loadApiKey()
         loadTimeout()
 
-        saveButton.setOnClickListener {
-            saveApiKey()
-            saveTimeout()
-        }
+        apiKeyEditText.addTextChangedListener { saveApiKey() }
+        timeoutEditText.addTextChangedListener { saveTimeout() }
     }
 
     private fun loadApiKey() {
@@ -58,12 +55,26 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     private fun loadTimeout() {
-        val timeout = sharedPreferences.getInt("TIMEOUT", 30)
+        val timeout = timeoutSharedPreferences.getInt("TIMEOUT", 0)
         timeoutEditText.setText(timeout.toString())
     }
 
     private fun saveTimeout() {
         val timeout = timeoutEditText.text.toString().toIntOrNull() ?: 0
-        sharedPreferences.edit().putInt("TIMEOUT", timeout).apply()
+        timeoutSharedPreferences.edit().putInt("TIMEOUT", timeout).apply()
+    }
+
+    private fun EditText.addTextChangedListener(afterTextChanged: (Editable) -> Unit) {
+        this.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                afterTextChanged(s)
+            }
+        })
     }
 }
